c97b0d33e6f55d37d77a1ae59b9893f55047c813
Loicsoh <www.sohloic132@gmail.com>
2025-02-03 15:34:44 +0100

    blagues

--- joke-app/script.js
+++ joke-app/script.js
@@ -1,15 +1,16 @@
 
 const headerBlague = document.getElementById("header")
 const textBlague = document.getElementById("content")
-
-fetch("https://api.blablagues.net/?rub=blagues")
+function Blague() {
+    fetch("https://api.blablagues.net/?rub=blagues")
 .then((resp) => resp.json())
 .then((info) => {
-    console.log(blague.data);
-
-    headerBlague.textContent = info.data.data.content.text_head
+    headerBlague.textContent = info.data.content.text_head
     textBlague.textContent = info.data.content.text
 })
+}
 
 Blague()
+
+btn.addEventListener('click', Blague)
 
