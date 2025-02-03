09bb51b74ebe7f88e210ceab95e050c99b1e4beb
Loicsoh <www.sohloic132@gmail.com>
2025-02-03 15:16:18 +0100

    kk

--- joke-app/script.js
+++ joke-app/script.js
@@ -1,4 +1,15 @@
-fetch('https://api.blablagues.net/?rub=blagues')
-.then((response) => {
-    console.log(response)
+
+const headerBlague = document.getElementById("header")
+const textBlague = document.getElementById("content")
+
+fetch("https://api.blablagues.net/?rub=blagues")
+.then((resp) => resp.json())
+.then((info) => {
+    console.log(blague.data);
+
+    headerBlague.textContent = info.data.data.content.text_head
+    textBlague.textContent = info.data.content.text
 })
+
+Blague()
+
