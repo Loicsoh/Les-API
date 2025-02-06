e567acb9fb600a2162bd3d0eba02ca8ab2ab76cd
Loicsoh <www.sohloic132@gmail.com>
2025-02-06 16:07:09 +0100

    mk

--- User-App/script.js
+++ User-App/script.js
@@ -1,23 +1,69 @@
+// let fetchUser1 = [];
+
+// const fetchUser1 = async () => {
+    
+//    await fetch("https://randomuser.me/api/?results=24")
+//     .then((res) => res.json())
+//     .then ((data) => console.log(data.results))
+
+
+// }
+// fetchUser1();
+
 let userDate = [];
 
-const fetchUser1 = () => {
+const fetchUser = async () => {
     
-    fetch("https://randomuser.me/api/?results=24")
+   await fetch("https://randomuser.me/api/?results=24")
     .then((res) => res.json())
-    .then ((data) => console.log(data.results))
-
-
+    .then ((data) => userDate = data.results)
+    console.log(userDate)
 }
-fetchUser();
 
-let userDate = [];
+const userDisplay = async () => {
+    await fetchUser()
 
-const fetchUser = () => {
-    
-    fetch("https://randomuser.me/api/?results=24")
-    .then((res) => res.json())
-    .then ((data) => console.log(data.results))
+    const dateParser = (date) => {
+        const newDate = new Date(date) .toLocaleDateString("fr-FR",{
+            year : "numeric",
+            month : "long",
+            day : "numeric",
+            hour : "numeric",
+            minute : "numeric",
+            second : "numeric"
+        })
+        return newDate
+    }
+
+
+    const calDay = (Date) => {
+        const today = newDate();
+        // console.log(today);
+
+        const todayTimestamp = Date.parse(today)
+        console.log(todayTimestamp); 
+        const timestamp = Date.parse(Date)
+        
+        return Math.floor( todayTimestamp - timestamp /(1000*60*60*24))
+    }
+
+    calDay()
+
+
+    document.body.innerHTML = userDate.map(
+        (user) => 
+             `
+            <div class="card">
+            <img src="${user.picture.large}" alt="La photo de ${user.name.last}">
+            <h4>${user.name.first} <span>${user.name.last}</span></h4>
+            <p> ${user.location.city}, ${dateParser(user.dob.date)}, a ${dateParser(user.dob.hour)}</p>
+            <i> Membre depuis ${user.registered.date} </i>
+            </div>
+            `
+        
+    ).join("\n")
 }
-fetchUser();
+
+userDisplay();
 
 
